2eb70a875df1cfe508308db6eafd2d91c692ec4d
masterJunsu <kofhoom@gmail.com>
2023-02-02 02:19:18 +0900

    지도 넓이 변경

--- js/js.js
+++ js/js.js
@@ -191,16 +191,25 @@ var mapContainer = document.getElementById("map"), // 지도를 표시할 div
   mapOption = {
     center: new kakao.maps.LatLng(37.56646, 126.98121), // 지도의 중심좌표
     level: 5, // 지도의 확대 레벨 디폴트 셋팅
-    mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
   };
 
 // 지도를 생성한다
 var map = new kakao.maps.Map(mapContainer, mapOption);
-// 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성
 
+// 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성
 var infowindow = new kakao.maps.InfoWindow({
   zIndex: 1,
 });
+
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 var currentPos; // 현재 위치를 담을 변수
 
